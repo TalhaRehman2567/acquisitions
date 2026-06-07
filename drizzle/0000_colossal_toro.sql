111a4176c68adbf166f9e4b21deca5b44f907d55
Talha Rehman <talharehman2567@gmail.com>
2026-06-07 23:57:08 +0500

    setup db

---
+++ drizzle/0000_colossal_toro.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar NOT NULL,
+	"email" varchar NOT NULL,
+	"password" varchar NOT NULL,
+	"role" varchar DEFAULT 'user' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
